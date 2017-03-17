9653bdada19ca6eef575b94824e9c6a01bd2902e
pwz <pwz1985@163.com>
2017-03-17 23:36:15 +0800

    首页显示

---
+++ src/modules/home/components/GoodsInfo.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import { Carousel, Flex } from 'antd-mobile';
+import { Img } from 'commonComponent';
+
+/**图片在上面，文字在下面的布局 */
+export function ImgGoodsInfo({ dataItem }) {
+  return <Flex direction='column' >
+      <Flex.Item style={{textAlign:'center'}}>
+        <Img src={dataItem.goodsImage} style={{ width: '80%', height:'1.5rem' }} />
+      </Flex.Item>
+      <Flex.Item>
+        <span style={{fontSize:'0.28rem',color:'gray'}}> {dataItem.goodsName}</span> 
+      </Flex.Item>
+      <Flex.Item>
+        <span style={{fontSize:'0.24rem',color:'red'}}>{`¥${dataItem.goodsPrice}`}</span>
+      </Flex.Item>
+    </Flex>
+}
+
+/**文字在上面，图片在下面的布局 */
+export function GoodsImgInfo({ dataItem }) {
+  return <Flex direction='column' >
+      <Flex.Item>
+        <span style={{fontSize:'0.28rem'}}> {dataItem.gcName}</span> 
+      </Flex.Item>
+      <Flex.Item>
+        <span style={{fontSize:'0.24rem',color:'gray'}}>{dataItem.goodsName}</span>
+      </Flex.Item>
+      <Flex.Item style={{textAlign:'center'}}>
+        <Img src={dataItem.goodsImage} style={{ width: '80%', height:'1.5rem' }} />
+      </Flex.Item>
+    </Flex>
+}
--- src/modules/home/components/HomeFloorGoods.js
+++ src/modules/home/components/HomeFloorGoods.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import { Grid, List, Flex } from 'antd-mobile';
+import { Grid, List, Flex, Icon } from 'antd-mobile';
 import { Img } from 'commonComponent';
-
+import { ImgGoodsInfo, GoodsImgInfo } from './GoodsInfo';
 
 const Layout28 = () => {}
 
@@ -16,25 +16,62 @@ const Layout33 = ({ goodsList }) => {
 class HomeFloorGoods extends React.PureComponent {
 
   renderHeader = (data) => {
-    return data.floorName
+    return <Flex>
+      <Flex.Item>{data.floorName}</Flex.Item>
+      <Flex.Item style={{textAlign:'right'}}><a href='#'>更多</a><Icon type='right' size='xs'></Icon></Flex.Item>
+    </Flex>
   }
 
   render() {
     const { data } = this.props;
-    console.log(data);
-    // if(data){
+    let firstBlock = null;
+    let sencondBlock = null;
 
-    // }
+    if (data.floorType == '2*8') {
+      firstBlock = <Grid data={data.goodsList.slice(0,2)} columnNum={2} hasLine={false}
+        onClick={this.onClick}
+          renderItem={(dataItem,index)=>(
+            <ImgGoodsInfo dataItem={dataItem}></ImgGoodsInfo>
+          )}>
+      </Grid>
+
+      sencondBlock = <Grid data={data.goodsList.slice(2,10)} columnNum={4} hasLine={false}
+        onClick={this.onClick}
+          renderItem={(dataItem,index)=>(
+            <ImgGoodsInfo dataItem={dataItem}></ImgGoodsInfo>
+          )}>
+      </Grid>
+    } else if (data.floorType == '9') {
+      firstBlock = <Grid data={data.goodsList} columnNum={3} hasLine={false}
+        onClick={this.onClick}
+          renderItem={(dataItem,index)=>(
+            <GoodsImgInfo dataItem={dataItem}></GoodsImgInfo>
+          )}>
+      </Grid>
+    } else {
+      firstBlock = <Grid data={data.goodsList.slice(0,2)} columnNum={4} hasLine={false}
+        onClick={this.onClick}
+          renderItem={(dataItem,index)=>(
+            <ImgGoodsInfo dataItem={dataItem}></ImgGoodsInfo>
+          )}>
+      </Grid>
+
+      sencondBlock = <Grid data={data.goodsList.slice(2,10)} columnNum={4} hasLine={false}
+        onClick={this.onClick}
+          renderItem={(dataItem,index)=>(
+            <ImgGoodsInfo dataItem={dataItem}></ImgGoodsInfo>
+          )}>
+      </Grid>
+    }
 
     return <List renderHeader={() => this.renderHeader(data) }>
       <List.Item>
-        <Flex>
-          <Flex.Item>
-            {/*<Img src={data}></Img>*/}
-          </Flex.Item>
-          <Flex.Item>2</Flex.Item>
-        </Flex>
+        {firstBlock}
       </List.Item>
+      {sencondBlock && <List.Item>
+        {sencondBlock}
+      </List.Item>
+      }
     </List>
   }
 }
