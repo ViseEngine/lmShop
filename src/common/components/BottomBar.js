ec04912af1ea1499829acd40241db82799580a41
pwz <pwz1985@163.com>
2017-03-18 12:02:02 +0800

    ff

--- src/common/components/BottomBar.js
+++ src/common/components/BottomBar.js
@@ -13,21 +13,8 @@ class BottomBar extends React.Component {
     };
   }
 
-  renderContent(pageText) {
-    return (
-      <div style={{ backgroundColor: 'white', height: '100%', textAlign: 'center' }}>
-        <div style={{ paddingTop: 60 }}>你已点击“{pageText}” tab， 当前展示“{pageText}”信息</div>
-        <a style={{ display: 'block', marginTop: 40, marginBottom: 600, color: '#108ee9' }} onClick={(e) => {
-          e.preventDefault();
-          this.setState({
-            hidden: !this.state.hidden,
-          });
-        }}
-        >
-          点击切换 tab-bar 显示/隐藏
-        </a>
-      </div>
-    );
+  changeTab = (type) => {
+    alert(type);
   }
 
   render() {
@@ -47,19 +34,7 @@ class BottomBar extends React.Component {
             background: 'url(http://bbc.leimingtech.com//res_v4.0/h5/images/b_1.png) center center /  0.42rem 0.42rem no-repeat' }}
           />
           }
-          selectedIcon={<div style={{
-            width: '0.44rem',
-            height: '0.44rem',
-            background: 'url(http://bbc.leimingtech.com//res_v4.0/h5/images/b_2.png) center center /  0.42rem 0.42rem no-repeat' }}
-          />
-          }
-          selected={this.state.selectedTab === 'blueTab'}
-          onPress={() => {
-            this.setState({
-              selectedTab: 'blueTab',
-            });
-          }}
-          data-seed="logId"
+          onPress={()=>this.changeTab('home')}
         >
         </TabBar.Item>
         <TabBar.Item
@@ -69,23 +44,10 @@ class BottomBar extends React.Component {
             background: 'url(http://bbc.leimingtech.com//res_v4.0/h5/images/b_3.png) center center /  0.42rem 0.42rem no-repeat' }}
           />
           }
-          selectedIcon={<div style={{
-            width: '0.44rem',
-            height: '0.44rem',
-            background: 'url(http://bbc.leimingtech.com//res_v4.0/h5/images/b_4.png) center center /  0.42rem 0.42rem no-repeat' }}
-          />
-          }
           title="分类"
           key="分类"
-          selected={this.state.selectedTab === 'redTab'}
-          onPress={() => {
-            this.setState({
-              selectedTab: 'redTab',
-            });
-          }}
-          data-seed="logId1"
+          onPress={()=>this.changeTab('goodsClass')}
         >
-          {/*{this.renderContent('口碑')}*/}
         </TabBar.Item>
         <TabBar.Item
           icon={
@@ -97,28 +59,16 @@ class BottomBar extends React.Component {
           }
           title="购物车"
           key="购物车"
-          selected={this.state.selectedTab === 'greenTab'}
-          onPress={() => {
-            this.setState({
-              selectedTab: 'greenTab',
-            });
-          }}
+          onPress={()=>this.changeTab('cart')}
         >
-          {/*{this.renderContent('朋友')}*/}
         </TabBar.Item>
         <TabBar.Item
           icon={{ uri: 'https://zos.alipayobjects.com/rmsportal/asJMfBrNqpMMlVpeInPQ.svg' }}
           selectedIcon={{ uri: 'https://zos.alipayobjects.com/rmsportal/gjpzzcrPMkhfEqgbYvmN.svg' }}
           title="我的"
           key="我的"
-          selected={this.state.selectedTab === 'yellowTab'}
-          onPress={() => {
-            this.setState({
-              selectedTab: 'yellowTab',
-            });
-          }}
+          onPress={()=>this.changeTab('my')}
         >
-          {/*{this.renderContent('我的')}*/}
         </TabBar.Item>
       </TabBar>
     );
